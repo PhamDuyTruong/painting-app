5b77f55d8449d99c5819b8d05bb4bc02d8918dcf
Truong <duytruong5437@gmail.com>
2022-09-19 12:06:59 +0700

    add logic

--- script.js
+++ script.js
@@ -34,3 +34,48 @@ const drawRect = (e) =>{
     }
     ctx.fillRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
 }
+
+const drawCircle = (e) =>{
+    ctx.beginPath();
+    let radius = Math.sqrt(Math.pow((prevMouseX - e.offsetX), 2) + Math.pow((prevMouseY - e.offsetY), 2));
+    ctx.arc(prevMouseX, prevMouseY, radius, 0, 2*Math.PI);
+    fillColor.checked ? ctx.fill() : ctx.stroke();
+};
+
+const drawTriangle = (e) =>{
+    ctx.beginPath();
+    ctx.moveTo(prevMouseX, prevMouseY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.lineTo(prevMouseX * 2 - e.offsetX, e.offsetY);
+    ctx.closePath();
+    fillColor.checked ? ctx.fill() : ctx.stroke();
+};
+
+const startDrawing = (e) =>{
+   isDraw = true;
+   prevMouseX = e.offsetX;
+   prevMouseY = e.offsetY;
+   ctx.beginPath();
+   ctx.lineWidth = brushWidth;
+   ctx.strokeStyle = selectedColor;
+   ctx.fillStyle = selectedColor;
+   snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
+};
+
+// Function to draw
+const drawing = (e) =>{
+    if(!isDraw) return;
+    ctx.putImageData(snapshot, 0, 0);
+
+    if(selectedTool === "brush" || selectedColor === "eraser"){
+        ctx.strokeStyle = selectedTool === "eraser" ? "#fff" : selectedColor;
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+    } else if(selectedTool === "rectangle"){
+        drawRect(e);
+    } else if(selectedTool === "circle"){
+        drawCircle(e);
+    }else{
+        drawTriangle(e);
+    }
+}
